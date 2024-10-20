c2370526288a7ee9996217c8892e46ba2e3507da
Rohit Sarpotdar <rohi7ns@gmail.com>
2024-10-20 18:04:45 +0530

    simplifed decoding opcode types added tests

--- src/riscv/opcode_type.zig
+++ src/riscv/opcode_type.zig
@@ -1,22 +1,20 @@
+const std = @import("std");
+const testing = std.testing;
+
 pub const RType = struct {
     rs2: u5,
     rs1: u5,
     rd: u5,
 
-    pub fn decode(raw_without_opcode: u25) .{ RType, u3, u7 } {
-        const funct3_bytes: u20 = raw_without_opcode >> 5;
-        const funct3: u3 = funct3_bytes & 0b111;
-        const rs1: u17 = funct3_bytes >> 3;
-        const rs2: u12 = rs1 >> 5;
-        const funct7: u7 = (rs2 >> 5) & 0b111_1111;
+    pub fn decode(raw: u32) struct { rtype: RType, funct3: u3, funct7: u7 } {
         return .{
-            .{
-                .rs2 = @intCast(rs2 & 0b1_1111),
-                .rs1 = @intCast(rs1 & 0b1_1111),
-                .rd = @intCast(raw_without_opcode & 0b1_1111),
+            .rtype = .{
+                .rs2 = @truncate((raw >> 20) & 0b1_1111),
+                .rs1 = @truncate((raw >> 15) & 0b1_1111),
+                .rd = @truncate((raw >> 7) & 0b1_1111),
             },
-            funct3,
-            funct7,
+            .funct3 = @truncate((raw >> 12) & 0b111),
+            .funct7 = @truncate((raw >> 25) & 0b111_1111),
         };
     }
 };
@@ -26,18 +24,14 @@ pub const IType = struct {
     rs1: u5,
     rd: u5,
 
-    pub fn decode(raw_without_opcode: u25) .{ IType, u3 } {
-        const funct3_bytes: u20 = raw_without_opcode >> 5;
-        const funct3: u3 = funct3_bytes & 0b111;
-        const rs1: u17 = funct3_bytes >> 3;
-        const imm: u12 = rs1 >> 5;
+    pub fn decode(raw: u32) struct { itype: IType, funct3: u3 } {
         return .{
-            .{
-                .imm = @intCast(imm),
-                .rs1 = @intCast(rs1 & 0b1_1111),
-                .rd = @intCast(raw_without_opcode & 0b1_1111),
+            .itype = .{
+                .imm = @truncate(raw >> 20),
+                .rs1 = @truncate((raw >> 15) & 0b1_1111),
+                .rd = @truncate((raw >> 7) & 0b1_1111),
             },
-            funct3,
+            .funct3 = @truncate((raw >> 12) & 0b111),
         };
     }
 };
@@ -47,20 +41,16 @@ pub const SType = struct {
     rs2: u5,
     rs1: u5,
 
-    pub fn decode(raw_without_opcode: u25) .{ SType, u3 } {
-        const imm4_0: u5 = raw_without_opcode & 0b1_1111;
-        const funct3_bytes: u20 = raw_without_opcode >> 5;
-        const funct3: u3 = funct3_bytes & 0b111;
-        const rs1: u17 = funct3_bytes >> 3;
-        const rs2: u12 = rs1 >> 5;
-        const imm11_5: u12 = rs2 & 0b1111_1110_0000;
+    pub fn decode(raw: u32) struct { stype: SType, funct3: u3 } {
+        const imm4_0 = (raw >> 7) & 0b1_1111;
+        const imm11_5 = (raw >> 25) & 0b1111_1110_0000;
         return .{
-            .{
-                .imm = @intCast(imm11_5 | imm4_0),
-                .rs2 = @intCast(rs2 & 0b1_1111),
-                .rs1 = @intCast(rs1 & 0b1_1111),
+            .stype = .{
+                .imm = @truncate(imm11_5 | imm4_0),
+                .rs2 = @truncate((raw >> 20) & 0b1_1111),
+                .rs1 = @truncate((raw >> 15) & 0b1_1111),
             },
-            funct3,
+            .funct3 = @truncate((raw >> 12) & 0b111),
         };
     }
 };
@@ -70,23 +60,18 @@ pub const BType = struct {
     rs2: u5,
     rs1: u5,
 
-    pub fn decode(raw_without_opcode: u25) .{ BType, u3 } {
-        const imm11: u11 = (raw_without_opcode & 0b1) << 10;
-        const imm4_1_bytes: u24 = raw_without_opcode >> 1;
-        const imm4_1: u4 = imm4_1_bytes & 0b1111;
-        const funct3: u20 = imm4_1_bytes >> 4;
-        const rs1: u17 = funct3 >> 3;
-        const rs2: u12 = rs1 >> 5;
-        const imm_bytes: u11 = rs2 >> 1;
-        const imm10_5: u10 = imm_bytes & 0b11_1111_0000;
-        const imm12: u12 = (imm_bytes & 0b100_0000_0000) << 1;
+    pub fn decode(raw: u32) struct { btype: BType, funct3: u3 } {
+        const imm11 = ((raw >> 7) & 0b1) << 10;
+        const imm4_1 = (raw >> 8) & 0b1111;
+        const imm10_5 = (raw >> 25) & 0b11_1111_0000;
+        const imm12 = raw >> 30;
         return .{
-            .{
-                .imm = @intCast(imm12 | imm11 | imm10_5 | imm4_1),
-                .rs2 = @intCast(rs2 & 0b1_1111),
-                .rs1 = @intCast(rs1 & 0b1_1111),
+            .btype = .{
+                .imm = @truncate(imm12 | imm11 | imm10_5 | imm4_1),
+                .rs2 = @truncate((raw >> 20) & 0b1_1111),
+                .rs1 = @truncate((raw >> 15) & 0b1_1111),
             },
-            (funct3 & 0b111),
+            .funct3 = ((raw >> 12) & 0b111),
         };
     }
 };
@@ -95,10 +80,10 @@ pub const UType = struct {
     imm: u20,
     rd: u5,
 
-    pub fn decode(raw_without_opcode: u25) UType {
+    pub fn decode(raw: u32) UType {
         return .{
-            .imm = @intCast(raw_without_opcode >> 5),
-            .rd = @intCast(raw_without_opcode & 0b1_1111),
+            .imm = @truncate(raw >> 12),
+            .rd = @truncate((raw >> 7) & 0b1_1111),
         };
     }
 };
@@ -107,15 +92,47 @@ pub const JType = struct {
     imm: u20,
     rd: u5,
 
-    pub fn decode(raw_without_opcode: u25) RType {
-        const imm19_12: u19 = (raw_without_opcode & 0b1_1111_1110_0000) << 6;
-        const imm11: u11 = (raw_without_opcode & 0b1_0000_0000_0000) >> 2;
-        const imm_bytes21_31: u11 = raw_without_opcode >> 14;
-        const imm10_1: u10 = imm_bytes21_31 & 0b11_1111_1111;
-        const imm20 = (imm_bytes21_31 & 0b100_0000_0000) << 9;
+    pub fn decode(raw: u32) RType {
+        const imm19_12 = ((raw >> 12) & 0b1111_1111) << 11;
+        const imm11 = ((raw >> 20) & 0b1) >> 10;
+        const imm10_1 = (raw >> 21) & 0b11_1111_1111;
+        const imm20 = (raw >> 31) << 19;
         return .{
-            .imm = @intCast(imm20 | imm19_12 | imm11 | imm10_1),
-            .rd = @intCast(raw_without_opcode & 0b1_1111),
+            .imm = @truncate(imm20 | imm19_12 | imm11 | imm10_1),
+            .rd = @truncate((raw >> 7) & 0b1_1111),
         };
     }
 };
+
+test "test R-Type" {
+    const actual = RType.decode(0b0000_0001_0110_0010_0000_1001_1011_0011);
+    try testing.expectEqual(0b1_0110, actual.rtype.rs2);
+    try testing.expectEqual(0b0_0100, actual.rtype.rs1);
+    try testing.expectEqual(0b1_0011, actual.rtype.rd);
+    try testing.expectEqual(0b000, actual.funct3);
+    try testing.expectEqual(0b000_0000, actual.funct7);
+}
+
+test "test I-Type" {
+    const actual = IType.decode(0b0000_0001_0110_0010_0000_1001_1011_0011);
+    try testing.expectEqual(0b1_0011, actual.itype.imm);
+    try testing.expectEqual(0b0_0100, actual.itype.rs1);
+    try testing.expectEqual(0b000, actual.funct3);
+    try testing.expectEqual(0b1_0110, actual.itype.rd);
+}
+
+test "test S-Type" {
+    const actual = SType.decode(0b0000_0001_0110_0010_0000_1001_1011_0011);
+    try testing.expectEqual(0b1_0011, actual.stype.imm);
+    try testing.expectEqual(0b1_0110, actual.stype.rs2);
+    try testing.expectEqual(0b0_0100, actual.stype.rs1);
+    try testing.expectEqual(0b000, actual.funct3);
+}
+
+test "test B-Type" {
+    const actual = BType.decode(0b0000_0001_0110_0010_0000_1001_1011_0011);
+    try testing.expectEqual(0b1_0011, actual.btype.imm);
+    try testing.expectEqual(0b1_0110, actual.btype.rs2);
+    try testing.expectEqual(0b0_0100, actual.btype.rs1);
+    try testing.expectEqual(0b000, actual.funct3);
+}
